cff33bf52264a7403d8d3f38fbc7cf13f98cecba
jamacoaudiovisual <jamacoaudiovisual@gmail.com>
2023-12-29 18:42:24 +0100

    Creación del proyecto

---
+++ src/main/java/vista/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vista;
+
+/**
+ *
+ * @author Javier Marcos Cobaleda
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+}
